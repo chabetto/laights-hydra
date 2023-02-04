89f1dbbb2e9d8b60af8c6067052004fc207978bd
chabetto <70392183+chabetto@users.noreply.github.com>
2023-02-04 17:17:54 +0000

    doesnt remake oscil when change

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ var hydra = new Hydra({
     enableStreamCapture: false,
 })
 
-function createOscillator(freq, scrollX, size, output, modulators, kaleid) {
-    let oscil = osc(freq,scrollX)
-    if (size != 0) { // size = 0 is sine, else square
-        oscil.thresh(size)
-    } if (kaleid) {
+function createOscillator(modulators, kaleid, no) { // modulators contain array of outputs
+    let oscil = osc(() => bigArrayForOsc[no-1][0],() => bigArrayForOsc[no-1][1])
+    oscil.thresh(() => bigArrayForOsc[no-1][2])
+    if (kaleid) {
         oscil.kaleid(99)
-    } if (modulators.length != 0) {
+    } if (modulators.length != 0) { // check if we are actually modulating
         for (let i = 0; i < modulators.length; i++){
             kaleid ? oscil.modulate(modulators[i]) : oscil.modulateRotate(modulators[i])
         }
     }
-    oscil.out(output)
+    let output = [o0,o1,o2][no - 1]
+    oscil.out(output) // output
 }
 
 function updateOsc(no) {
@@ -28,17 +28,16 @@ function updateOsc(no) {
     } if (document.getElementById("os" + no + "mod3").checked) {
         modArray.push(o2)
     }
-    // output
-    let output = [o0,o1,o2][no - 1]
-    // freq
-    let freq = document.getElementById("os" + no + "freq").valueAsNumber
-    // scroll
-    let scro = document.getElementById("os" + no + "scro").valueAsNumber
-    // size
-    let size = document.getElementById("os" + no + "size").valueAsNumber
     // kaleidoscope
     let kal = document.getElementById("os" + no + "kaleid").checked
-    createOscillator(freq,scro,size,output,modArray,kal)
+    createOscillator(modArray,kal,no)
+}
+
+function updateBigArray(no) {
+    bigArrayForOsc[no-1][0] = document.getElementById("os" + no + "freq").valueAsNumber
+    bigArrayForOsc[no-1][1] = document.getElementById("os" + no + "scro").valueAsNumber
+    bigArrayForOsc[no-1][2] = document.getElementById("os" + no + "size").valueAsNumber
+
 }
 
 function setUpOsc(no) {
@@ -53,9 +52,9 @@ function setUpOsc(no) {
     mod1.onclick = () => updateOsc(no)
     mod2.onclick = () => updateOsc(no)
     mod3.onclick = () => updateOsc(no)
-    size.onchange = () => updateOsc(no)
-    scro.onchange = () => updateOsc(no)
-    freq.onchange = () => updateOsc(no)
+    size.oninput = () => updateBigArray(no)
+    scro.oninput = () => updateBigArray(no)
+    freq.oninput = () => updateBigArray(no)
 }
 
 function start() {
@@ -66,7 +65,7 @@ function start() {
     document.getElementById("upload").addEventListener("change", playAudio, false);
 }
 
-function playAudio(stopTime = false) {
+function playAudio() {
     const uploadedFile = document.getElementById("upload").files[0];
     $("#src").attr("src", URL.createObjectURL(uploadedFile));
     document.getElementById("audio").load();
@@ -75,7 +74,7 @@ function playAudio(stopTime = false) {
     const source = audioContext.createMediaElementSource(htmlAudioElement);
     source.connect(audioContext.destination);
 
-    const freqOsc3 = document.getElementById("os3scro")
+    const freqOsc3 = document.getElementById("os3size")
 
     if (typeof Meyda === "undefined") {
     console.log("Meyda could not be found! Have you included it?");
@@ -84,19 +83,17 @@ function playAudio(stopTime = false) {
     const analyzer = Meyda.createMeydaAnalyzer({
         "audioContext": audioContext,
         "source": source,
-        "bufferSize": 4096*2,
+        "bufferSize": 512,
         "featureExtractors": ["rms"],
         "callback": features => {
         console.log(features);
-        freqOsc3.value = features.rms * 30;
+        freqOsc3.value = features.rms * 3;
+        bigArrayForOsc[2][2] = features.rms * 3;
         }
     });
     analyzer.start();
     }
-
-    let gameTime = window.setInterval(() => {
-        updateOsc(3)
-    },  100);
 }
+let bigArrayForOsc = [[5,1,0.01],[5,1,0.01],[5,1,0.01]]
 
 start()
